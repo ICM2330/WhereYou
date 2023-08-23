470ffe01cb4fefedb68803f694ba9c146e5df067
MrLepsss <lepeleyj@javeriana.edu.co>
2023-08-22 20:07:05 -0500

    HomeActivity
    Layout y funcionalidades de los botones del HomeActivity. Espero que nada se rompa todo lo probe aqui en mi PC pls si el programa muere mi culpa es por mucho circustancial.

--- app/src/main/java/com/example/whereyou/HomeActivity.kt
+++ app/src/main/java/com/example/whereyou/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.whereyou
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.whereyou.databinding.ActivityHomeBinding
@@ -10,5 +11,41 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+		
+		binding.HABorrar.setOnClickListener {
+            binding.HABusquedaUbicacion.setText("")
+        }
+
+        binding.HAOpciones.setOnClickListener {
+
+        }
+
+        binding.HAMicrofono.setOnClickListener {
+
+        }
+
+        binding.HAUbicacion.setOnClickListener {
+            
+        }
+
+        binding.HAGrupos.setOnClickListener {
+            startActivity(Intent(baseContext,GruposActivity::class.java))
+        }
+
+        binding.HAChats.setOnClickListener {
+            //startActivity(Intent(baseContext,ChatsActivity::class.java))
+        }
+
+        binding.HAHome.setOnClickListener {
+            startActivity(Intent(baseContext,HomeActivity::class.java))
+        }
+
+        binding.HAAlertas.setOnClickListener {
+            //startActivity(Intent(baseContext,AlertasActivity::class.java))
+        }
+
+        binding.HAPerfil.setOnClickListener {
+            //startActivity(Intent(baseContext,PerfilActivity::class.java))
+        }
     }
 }
