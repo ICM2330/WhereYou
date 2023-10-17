15e280362b802c3aba43258e31a17ab82ec94e2b
MrLepsss <lepeleyj@javeriana.edu.co>
2023-10-17 18:17:09 -0500

    MagneticField completado
    Odio la rotación de objetos y odio a Azimuth

--- app/src/main/java/com/example/whereyou/services/MagneticFieldSensorService.kt
+++ app/src/main/java/com/example/whereyou/services/MagneticFieldSensorService.kt
@@ -5,27 +5,34 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.util.Log
 
 class MagneticFieldSensorService (context: Context){
     private var sensorManager: SensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     private var magneticFieldSensor: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
     private var magneticFieldEventListener: SensorEventListener? = null
-    private val rotationMatrix = FloatArray(9)
-    private val orientationValues = FloatArray(3)
+    private val azimuthValues = FloatArray(3)
 
     fun registerMagneticFieldSensorListener(listener:(rotation: Float)->Unit){
         magneticFieldEventListener = object : SensorEventListener {
             override fun onSensorChanged(event: SensorEvent?) {
                 if (event != null && event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
                     // Obtén la matriz de rotación y la orientación
-                    SensorManager.getRotationMatrix(rotationMatrix, null, event.values, event.values)
+                    System.arraycopy(event.values, 0, azimuthValues, 0, 3)
 
-                    // Convierte el ángulo en radianes y luego a grados
-                    val azimuthInRadians = SensorManager.getOrientation(rotationMatrix, orientationValues)[0]
-                    val azimuthInDegrees = Math.toDegrees(azimuthInRadians.toDouble()).toFloat()
+                    val azimuth = azimuthValues[0]
+                    var adjustedAzimuth = 0.0
 
+                    // Asegurarse de que el ángulo esté en el rango de 0 a 180 grados
+                    if(azimuth < 0){
+                        adjustedAzimuth = ((azimuth - 180) % 180).toDouble()
+                    }
+                    else{
+                        adjustedAzimuth = ((azimuth + 180) % 180).toDouble()
+                    }
+                    Log.i("MF", "Grados: $adjustedAzimuth")
                     // Devuelve la rotación en el eje Z encontrada
-                    listener(-azimuthInDegrees)
+                    listener(adjustedAzimuth.toFloat())
                 }
 
             }
